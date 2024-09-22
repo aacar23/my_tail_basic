37980506182a3948aaa52906216cab179a7b32ae
Ahmet ACAR <aacarbiz@protonmail.com>
2024-09-22 12:54:16 +0300

    Change the handling of file or folder not existing with checking errno condition and displaying appropriate message.

--- my_tail_basic.c
+++ my_tail_basic.c
@@ -9,15 +9,26 @@
 
 void my_tail_basic(const char *file_name, ssize_t count, int print_head, void (*file_pos_mover)(FILE *file_handle, long offset, int origin))
 {
+    errno = 0;
+
     if (!count)
         return;
 
-    if (print_head)
-        printf("==> %s <==\n", file_name);
+
 
     FILE *file_handle = fopen(file_name, "r+");
-    if (!file_handle)
+    if (!file_handle){
+
+        if (errno == ENOENT){
+            printf("my_tail_basic: cannot open \'%s\' for reading: %s\n", file_name, strerror(errno));
+            return;
+        }
+
         error_exit("%s:%s:%s:%s", __func__, "fopen", strerror(errno), time_to_str());
+    }
+
+    if (print_head)
+        printf("==> %s <==\n", file_name);
 
     (*file_pos_mover)(file_handle, count, count < 0 ? SEEK_END : SEEK_SET);
 
